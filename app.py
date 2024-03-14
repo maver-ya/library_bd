2472fa57e5340445491d33a0569ea3ef0ab1f62f
maver-ya <maver2005@ya.ru>
2024-03-14 17:49:08 +0300

    first

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST'] = 'std-2609.ist.mospolytech.ru:22'
+app.config['MYSQL_DATABASE_USER'] = 'std'
+app.config['MYSQL_DATABASE_USER'] = 'Cerf67789$'
+app.config['MYSQL_DATABASE_DB'] = 'std_2609_home_library'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def db_req():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Authors")
+    result = cur.fetchall()
+    cur.close
+    return str(result)
+
+
+if __name__ == '__main__':
+    app.run()
